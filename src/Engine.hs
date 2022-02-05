5dc9804189bf0be85ce31bc3bf05b5703c132ed5
Cornelius Sevald-Krause <cornelius.sevald@protonmail.com>
2022-02-05 19:12:16 +0100

    Improve guessing function
    Guessing with repeating letters will only mark match the number
    of letters in the word to be guessed.
    
    As an example, if the word is "apple" and the guess is "pasta",
    only the first 'a' will be marked as correct but in the wrong position
    and not both 'a's.
    
    Letters in the correct position will have priority,
    so if the word again is "apple" and the guess is "holly",
    the second 'l' will be marked as in the correct position and the
    first 'l' will be marked as not in the word.

--- src/Engine.hs
+++ src/Engine.hs
@@ -2,6 +2,7 @@
 
 module Engine where
 
+import Data.List (elemIndex)
 import System.Random
 
 data GuessType = NotInWord | WrongSpot | CorrectSpot | Default
@@ -28,9 +29,22 @@ isCorrectWord :: String -> [String] -> Bool
 isCorrectWord = elem
 
 guess :: String -> String -> Guess
-guess g w = zipWith (curry f) g w
-  where
-    f (gc, c) = (gc,) $ case (c == gc, gc `elem` w) of
-      (True, _) -> CorrectSpot
-      (_, True) -> WrongSpot
-      (_, False) -> NotInWord
+guess guess word
+  = zip guess $ zipWith max perfects corrects
+    where perfects    = zipWith (\c g -> if c == g then CorrectSpot else NotInWord) word guess
+          corrects    = go word' guess'
+          word'       = zipWith (\c p -> if p == CorrectSpot then Nothing else Just c) word perfects
+          guess'      = zipWith (\g p -> if p == CorrectSpot then Nothing else Just g) guess perfects
+          go c []     = []
+          go c (g:gs) =
+            case (g, elemIndex g c) of
+              (Nothing, _)  -> NotInWord : go c gs
+              (_, Nothing)  -> NotInWord : go c gs
+              (_, Just idx) -> WrongSpot : go c' gs
+                where c' = setAt c idx Nothing
+
+{- | 'setAt' @xs i x@ replaces the element at index @i@
+in list @xs@ with value @x@.
+-}
+setAt :: [a] -> Int -> a -> [a]
+setAt xs i x = take i xs ++ [x] ++ drop (i + 1) xs
